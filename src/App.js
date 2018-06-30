cdaa2340383616ed17ff7bd9cfec783db0c9bdb0
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-06-30 01:50:43 -0700

    styling and refresh button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Canvas from './Components/Canvas';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
         width: 700,
         height: 700,
@@ -12,7 +12,12 @@ class App extends Component {
         colors: ["#FF0000", "#FFFF00", "#0000FF"],
         opacity: 0.1,
         maxRadius: 300,
+        update: false,
     }
+  }
+
+  shouldComponentUpdate = () => {
+    return false;
   }
   
   handleColorChange = event => {
@@ -27,28 +32,30 @@ class App extends Component {
     this.setState({[target]: event.target.value});
   }
 
+  handleRefresh = () => {
+    this.forceUpdate();
+  }
+
   render(){
     return(
       <div className="App">
         <div className="controls">
-          <div className="colors">
-            <p>Colors:</p>
-            <input type="color" id="0" defaultValue={this.state.colors[0]} onChange={this.handleColorChange}/>
-            <input type="color" id="1" defaultValue={this.state.colors[1]} onChange={this.handleColorChange}/>
-            <input type="color" id="2" defaultValue={this.state.colors[2]} onChange={this.handleColorChange}/>
-          </div>
-          <div className="numbers">
-            <p>Width:</p>
-            <input type="number" id="width" defaultValue={this.state.width} onChange={this.handleNumberChange}/>
-            <p>Height:</p>
-            <input type="number" id="height" defaultValue={this.state.height} onChange={this.handleNumberChange}/>
-            <p>Opacity:</p>
-            <input type="number" id="opacity" defaultValue={this.state.opacity} onChange={this.handleNumberChange}/>
-            <p>Max Radius:</p>
-            <input type="number" id="maxRadius" defaultValue={this.state.maxRadius} onChange={this.handleNumberChange}/>
-            <p>Number of Circles:</p>
-            <input type="number" id="loops" defaultValue={this.state.loops} onChange={this.handleNumberChange}/>
-          </div>
+          <h1>Circle-Gen</h1>
+          <button onClick={this.handleRefresh}>Refresh</button><br/>
+          <h2>Colors:</h2>
+            <input type="color" id="0" className="input" defaultValue={this.state.colors[0]} onChange={this.handleColorChange}/><br/>
+            <input type="color" id="1" className="input" defaultValue={this.state.colors[1]} onChange={this.handleColorChange}/><br/>
+            <input type="color" id="2" className="input" defaultValue={this.state.colors[2]} onChange={this.handleColorChange}/><br/>
+          <h2>Width:</h2>
+            <input type="text" id="width" className="input" defaultValue={this.state.width} onChange={this.handleNumberChange} size="7"/>
+          <h2>Height:</h2>
+            <input type="text" id="height" className="input" defaultValue={this.state.height} onChange={this.handleNumberChange} size="7"/>
+          <h2>Opacity:</h2>
+            <input type="text" id="opacity" className="input" defaultValue={this.state.opacity} onChange={this.handleNumberChange} size="7"/>
+          <h2>Max Radius:</h2>
+            <input type="text" id="maxRadius" className="input" defaultValue={this.state.maxRadius} onChange={this.handleNumberChange} size="7"/>
+          <h2># of Circles:</h2>
+            <input type="text" id="loops" className="input" defaultValue={this.state.loops} onChange={this.handleNumberChange} size="7"/><br/>
         </div>
         <Canvas
           colors={this.state.colors}
